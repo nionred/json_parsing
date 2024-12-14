c9779919a5b62d039d11a00c5a99f76d972a57f9
nionred <rahman2015011@stud.kuet.ac.bd>
2024-12-14 17:01:42 +0600

    assignment on json parsing

---
+++ json parsing/json parsing/json_parsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_parsingApp.swift
+//  json parsing
+//
+//  Created by Mahir Azmain Haque on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct json_parsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
